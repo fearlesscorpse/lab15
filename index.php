f55aa684fc460ef70b9d59995bfbab9f166c2dd7
fearlesscorpse <baranova.karina4@yandex.ru>
2026-04-13 15:17:45 +0300

    Выполнено 3 задание

--- index.php
+++ index.php
@@ -8,4 +8,11 @@
         //Задание 2
         abstract public function infoAbout();
     }
+    //Задание 3
+    class Rectangle extends Figure {
+    }
+    class Triangle extends Figure {
+    }
+    class Square extends Figure {
+    }
 ?>
